0248ac69ae118d0d6c0c38ed6edff6c4d3a8ea5c
Дарья Высоцкая <da.vysotskaya@mail.ru>
2026-03-11 17:58:55 +0300

    Добавлены файлы из projects_3

---
+++ projects_3/task_3_8/age.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+readonly CURRENT_YEAR=2026
+readonly BIRTH_YEAR=2007
+AGE=$((CURRENT_YEAR - BIRTH_YEAR))
+echo "Текущий год: $CURRENT_YEAR"
+echo "Ваш примерный возраст: $AGE"
---
+++ projects_3/task_3_8/impulse.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+read -p "Имя гена:" name_gene
+read -p "Уровень экспрессии гена (целое число):" number
+echo "Экспрессия гена $name_gene составляет $number единиц"
